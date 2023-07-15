a70655bf156da221ded323434d928df92186445b
jonasalberton <jonasceolin@gmail.com>
2023-07-15 09:35:00 -0300

    initial

---
+++ cypress/cypress/integration/primeiro.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress"/>
+
+describe('template spec', () => {
+    it('Should visit page', () => {
+      cy.visit('https://wcaquino.me/cypress/componentes.html');
+      
+    })
+  })
